7874b1c62ca42ceada447b54147f3b946c5c1f46
muffafa <mm.savar@gmail.com>
2023-03-23 09:47:35 +0300

    latin font removed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
 import Head from "next/head";
-import { Inter } from "next/font/google";
 import Header from "@/components/header";
 import Hero from "@/components/hero";
 import Movies from "@/components/movies";
 import Matrix from "@/components/matrix";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (

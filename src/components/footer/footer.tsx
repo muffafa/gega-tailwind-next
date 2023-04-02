f228f91865ec79078855c8779eb67125100de98d
muffafa <mm.savar@gmail.com>
2023-04-02 11:52:28 +0300

    space removed

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer(){
             <div className="container flex flex-col space-y-6 md:space-y-0 md:flex-row px-10 lg:px-0 pb-8 text-s md:text-base">
                 <div className="basis-1/3">
                     <Logo className="text-2xl lg:text-3xl"/>
-                    <p className="text-sm mt-2">2023 ZENCİR SOFTWARE &copy; TÜM HAKLARI SAKLIDIR </p>
+                    <p className="text-sm mt-2">2023 ZENCİR SOFTWARE &copy; TÜM HAKLARI SAKLIDIR</p>
                     <p></p>
                 </div>
                 <div className="basis-1/3">
